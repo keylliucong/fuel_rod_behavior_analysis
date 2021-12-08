fcf9188e28dff965ec20173e04096b2ed4125b45
keylliucong <1305428189@qq.com>
2021-12-08 14:39:27 +0800

    简化程序，将热工水力模块瞬态温度计算中，多余代码精简

--- liucong-Fcode/liucong-Fcode/thermal hydraulic.f90
+++ liucong-Fcode/liucong-Fcode/thermal hydraulic.f90
@@ -29,11 +29,11 @@
         p_line(i)=p_line_max*sin(x+(i-1)*(pi-2*x)/(1.0*(n_axis-1)))
     end do
     do i=1,n_axis
-    a=0
-    b=0
-    a=(i-1)*cladding_length/((n_axis-1)*1.0)
-    b=(i-2)*cladding_length/((n_axis-2)*1.0)
-    Q_line(i)=p_line_max*cladding_length/(pi-2*x)*(cos(x+b*(pi-2*x)/cladding_length)-cos(x+a*(pi-2*x)/cladding_length))
+        a=0
+        b=0
+        a=(i-1)*cladding_length/((n_axis-1)*1.0)
+        b=(i-2)*cladding_length/((n_axis-2)*1.0)
+        Q_line(i)=p_line_max*cladding_length/(pi-2*x)*(cos(x+b*(pi-2*x)/cladding_length)-cos(x+a*(pi-2*x)/cladding_length))
     end do
     end subroutine
     !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!功率计算结束!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
@@ -180,8 +180,9 @@
     !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!温度计算瞬态!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
     subroutine T_transient_calculation(time11,n_a)
     integer i,time11,n_a
-    dimension A(n_radial+2,n_radial+2),B(n_radial+2),X(n_radial+2),Tc(n_radial+2),transient(n_radial+2)
-    double precision A,B,X,transient
+    dimension A(n_radial+2,n_radial+2),B(n_radial+2),X(n_radial+2)
+    dimension Tc(n_radial+2),transient(n_radial+2),Temperature_mid(t_number,n_axis,n_radial+3)
+    double precision A,B,X,transient,Temperature_mid
     real(8)::tt,rw,re,d_rw,d_re,heat
     cladding_T_surface=Temperature_transient(time11,n_a,n_radial+3)+p_line(n_a)&
         &/(pi*d(time11,n_a,n_radial+2)*h_coefficient(time11,n_a))
@@ -202,73 +203,56 @@
     tt=100.
     A(:,:)=0.
     do while(tt>0.01)
+
+
+
         do i=1,n_radial
             call fuel_coefficient_calculation(time11,n_a,i)
         end do
+
+
+
+
         !!!!!!!系数矩阵赋值开始!!!!!!!!
-        if (time11==1) then
-            transient(1)=UO2_denstiy*UO2_Cp*(d(time11,n_a,2)/4.)**2./AT/2.
-            A(1,1)=-(1.*k_fuel(time11,n_a,1)/2.+transient(1))
-            A(1,2)=1.*k_fuel(time11,n_a,1)/2.
-            B(1)=-(0.5*P_SQUARE(time11,n_a,1)*(d(time11,n_a,2)/4.)**2.+transient(1)*Temperature(time,n_a,1))
-            do i=2,n_radial+1
-                rw=(d(time11,n_a,i)+d(time11,n_a,i-1))/4.
-                re=(d(time11,n_a,i)+d(time11,n_a,i+1))/4.
-                d_rw=(d(time11,n_a,i)-d(time11,n_a,i-1))/2.
-                d_re=(d(time11,n_a,i+1)-d(time11,n_a,i))/2.
-                if (i<n_radial) then
-                    transient(i)=UO2_density*UO2_Cp/AT*(re-rw)*(re+rw)/2.
-                    A(i,i-1)=rw/d_rw*k_fuel(time11,n_a,i-1)
-                    A(i,i+1)=re/d_re*k_fuel(time11,n_a,i)
-                    A(i,i)=-(A(i,i-1)+A(i,i+1)+transient(i))
-                    B(i)=-1.*(P_SQUARE(time11,n_a,i)*(re-rw)*(re+rw)/2.+transient(i)*Temperature(time,n_a,i))
-                else if (i==n_radial) then
-                    A(i,i-1)=k_fuel(time11,n_a,i)/d_re
-                    A(i,i+1)=Kg(coolant_kind)
-                    A(i,i)=-(A(i,i-1)+A(i,i+1))
-                    B(i)=0
-                else if (i==n_radial+1) then
-                    A(i,i-1)=Kg(coolant_kind)*d(time11,n_a,n_radial)/d(time11,n_a,n_radial+1)
-                    A(i,i+1)=Kc(Temperature_transient(time11,n_a,i),cladding_T_surface)/d_rw
-                    A(i,i)=-(A(i,i-1)+A(i,i+1))
-                    B(i)=0
-                end if
+        do i=1,n_radial+3
+            if (time11==1) then
 
-            end do
-            A(n_radial+2,n_radial+2)=1.
-            B(n_radial+2)=cladding_T_surface
-        else
-            transient(1)=UO2_denstiy*UO2_Cp*(d(time11,n_a,2)/4.)**2./AT/2.
-            A(1,1)=-(1.*k_fuel(time11,n_a,1)/2.+transient(1))
-            A(1,2)=1.*k_fuel(time11,n_a,1)/2.
-            B(1)=-(0.5*P_SQUARE(time11,n_a,1)*(d(time11,n_a,2)/4.)**2.+transient(1)*Temperature_transient(time11-1,n_a,1))
-            do i=2,n_radial+1
-                rw=(d(time11,n_a,i)+d(time11,n_a,i-1))/4.
-                re=(d(time11,n_a,i)+d(time11,n_a,i+1))/4.
-                d_rw=(d(time11,n_a,i)-d(time11,n_a,i-1))/2.
-                d_re=(d(time11,n_a,i+1)-d(time11,n_a,i))/2.
-                if (i<n_radial) then
-                    transient(i)=UO2_density*UO2_Cp/AT*(re-rw)*(re+rw)/2.
-                    A(i,i-1)=rw/d_rw*k_fuel(time11,n_a,i-1)
-                    A(i,i+1)=re/d_re*k_fuel(time11,n_a,i)
-                    A(i,i)=-(A(i,i-1)+A(i,i+1)+transient(i))
-                    B(i)=-1.*(P_SQUARE(time11,n_a,i)*(re-rw)*(re+rw)/2.+transient(i)*Temperature_transient(time11-1,n_a,i))
-                else if (i==n_radial) then
-                    A(i,i-1)=k_fuel(time11,n_a,i)/d_re
-                    A(i,i+1)=Kg(coolant_kind)
-                    A(i,i)=-(A(i,i-1)+A(i,i+1))
-                    B(i)=0
-                else if (i==n_radial+1) then
-                    A(i,i-1)=Kg(coolant_kind)*d(time11,n_a,n_radial)/d(time11,n_a,n_radial+1)
-                    A(i,i+1)=Kc(Temperature_transient(time11,n_a,i),cladding_T_surface)/d_rw
-                    A(i,i)=-(A(i,i-1)+A(i,i+1))
-                    B(i)=0
-                end if
+                Temperature_mid(time11,n_a,i)=Temperature(time,n_a,i)
+            else
+                Temperature_mid(time11,n_a,i)=Temperature_transient(time11-1,n_a,i)
+            end if
+        end do
+        transient(1)=UO2_denstiy*UO2_Cp*(d(time11,n_a,2)/4.)**2./AT/2.
+        A(1,1)=-(1.*k_fuel(time11,n_a,1)/2.+transient(1))
+        A(1,2)=1.*k_fuel(time11,n_a,1)/2.
+        B(1)=-(0.5*P_SQUARE(time11,n_a,1)*(d(time11,n_a,2)/4.)**2.+transient(1)*Temperature_mid(time11,n_a,1))
+        do i=2,n_radial+1
+            rw=(d(time11,n_a,i)+d(time11,n_a,i-1))/4.
+            re=(d(time11,n_a,i)+d(time11,n_a,i+1))/4.
+            d_rw=(d(time11,n_a,i)-d(time11,n_a,i-1))/2.
+            d_re=(d(time11,n_a,i+1)-d(time11,n_a,i))/2.
+            if (i<n_radial) then
+                transient(i)=UO2_density*UO2_Cp/AT*(re-rw)*(re+rw)/2.
+                A(i,i-1)=rw/d_rw*k_fuel(time11,n_a,i-1)
+                A(i,i+1)=re/d_re*k_fuel(time11,n_a,i)
+                A(i,i)=-(A(i,i-1)+A(i,i+1)+transient(i))
+                B(i)=-1.*(P_SQUARE(time11,n_a,i)*(re-rw)*(re+rw)/2.+transient(i)*Temperature_mid(time11,n_a,i))
+            else if (i==n_radial) then
+                A(i,i-1)=k_fuel(time11,n_a,i)/d_re
+                A(i,i+1)=Kg(coolant_kind)
+                A(i,i)=-(A(i,i-1)+A(i,i+1))
+                B(i)=0
+            else if (i==n_radial+1) then
+                A(i,i-1)=Kg(coolant_kind)*d(time11,n_a,n_radial)/d(time11,n_a,n_radial+1)
+                A(i,i+1)=Kc(Temperature_transient(time11,n_a,i),cladding_T_surface)/d_rw
+                A(i,i)=-(A(i,i-1)+A(i,i+1))
+                B(i)=0
+            end if
 
-            end do
-            A(n_radial+2,n_radial+2)=1.
-            B(n_radial+2)=cladding_T_surface
-        end if
+        end do
+        A(n_radial+2,n_radial+2)=1.
+        B(n_radial+2)=cladding_T_surface
+
         !!!!!!!系数矩阵赋值结束!!!!!!!!
         call TDMA(n_radial+2,A,B,X)                            !!!!!TDMA计算，后续继续采取该计算迭代芯块温度值
         do i=1,n_radial+2
@@ -287,47 +271,36 @@
     !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!计算储热
     Tc(:)=0.
     heat=0.
-    if (time11==1) then
-        do i=1,n_radial+2
-            Tc(i)=Temperature_transient(time11,n_a,i)-Temperature(time,n_a,i)
-            if (i==1) then
-                heat=heat+Tc(i)*UO2_Cp*UO2_density*pi*((d(time11,n_a,i)/2.)**2)/2.
-            else if(i<n_radial) then
-                rw=(d(time11,n_a,i)+d(time11,n_a,i-1))/4.
-                re=(d(time11,n_a,i)+d(time11,n_a,i+1))/4.
-                heat=heat+Tc(i)*UO2_Cp*UO2_density*pi*(re**2-rw**2)
-            else if(i==n_radial) then
-                rw=(d(time11,n_a,i)+d(time11,n_a,i-1))/4.
-                heat=heat+Tc(i)*UO2_Cp*UO2_density*pi*(d(time11,n_a,i)**2/4.-rw**2)
-            else
-                rw=d(time11,n_a,n_radial+1)/2.
-                re=d(time11,n_a,n_radial+2)/2.
-                heat=heat+Tc(i)*UO2_Cp*UO2_density*pi*(re**2-rw**2)/2.
-            end if
-        end do
-    else
-        do i=1,n_radial+2
-            Tc(i)=Temperature_transient(time11,n_a,i)-Temperature_transient(time11-1,n_a,i)
-            if (i==1) then
-                heat=heat+Tc(i)*UO2_Cp*UO2_density*pi*((d(time11,n_a,i)/2.)**2)/2.
-            else if(i<n_radial) then
-                rw=(d(time11,n_a,i)+d(time11,n_a,i-1))/4.
-                re=(d(time11,n_a,i)+d(time11,n_a,i+1))/4.
-                heat=heat+Tc(i)*UO2_Cp*UO2_density*pi*(re**2-rw**2)
-            else if(i==n_radial) then
-                rw=(d(time11,n_a,i)+d(time11,n_a,i-1))/4.
-                heat=heat+Tc(i)*UO2_Cp*UO2_density*pi*(d(time11,n_a,i)**2/4.-rw**2)
-            else
-                rw=d(time11,n_a,n_radial+1)/2.
-                re=d(time11,n_a,n_radial+2)/2.
-                heat=heat+Tc(i)*UO2_Cp*UO2_density*pi*(re**2-rw**2)/2.
-            end if
-        end do
-    end if
+    do i=1,n_radial+3
+        if (time11==1) then
+
+            Temperature_mid(time11,n_a,i)=Temperature(time,n_a,i)
+        else
+            Temperature_mid(time11,n_a,i)=Temperature_transient(time11-1,n_a,i)
+        end if
+    end do
+    do i=1,n_radial+2
+        Tc(i)=Temperature_transient(time11,n_a,i)-Temperature_mid(time11,n_a,i)
+        if (i==1) then
+            heat=heat+Tc(i)*UO2_Cp*UO2_density*pi*((d(time11,n_a,i)/2.)**2)/2.
+        else if(i<n_radial) then
+            rw=(d(time11,n_a,i)+d(time11,n_a,i-1))/4.
+            re=(d(time11,n_a,i)+d(time11,n_a,i+1))/4.
+            heat=heat+Tc(i)*UO2_Cp*UO2_density*pi*(re**2-rw**2)
+        else if(i==n_radial) then
+            rw=(d(time11,n_a,i)+d(time11,n_a,i-1))/4.
+            heat=heat+Tc(i)*UO2_Cp*UO2_density*pi*(d(time11,n_a,i)**2/4.-rw**2)
+        else
+            rw=d(time11,n_a,n_radial+1)/2.
+            re=d(time11,n_a,n_radial+2)/2.
+            heat=heat+Tc(i)*UO2_Cp*UO2_density*pi*(re**2-rw**2)/2.
+        end if
+    end do
+
 
 
     do i=2,n_axis
-    Q_line(i)=Q_line(i)-heat*length_every        !热量更新
+        Q_line(i)=Q_line(i)-heat*length_every        !热量更新
     end do
     heat_dv=heat-heat_old                       !热量差值
 
